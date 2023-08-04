9ff53c6ae3d9fbef1f6aa4a6bfc4157999ac4a72
ihatescholz <daniel_wang_rw@hotmail.com>
2023-08-04 02:15:48 -0400

    - Added SQLAlchemy     Execute pip install -U Flask-SQLAlchemy to install     If broken, change interpreter - Added User database for testing

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
+# Create app
 app = Flask(__name__)
+# Create SQL Extension
+db = SQLAlchemy()
+# Configure database
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///user.db"
+# Initialize app with extension
+db.init_app(app)
+
+# Define Model
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String,unique=True,nullable=False)
+    email = db.Column(db.String)
+    
+# Create table
+with app.app_context():
+    db.create_all()
 
 @app.route("/")
 def index():
